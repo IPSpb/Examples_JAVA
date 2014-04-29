82c2909dae91bc63fd84841dcdea909805308220
Igor <warrior84spb@gmail.com>
2014-04-29 17:05:46 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import array.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Array ar = new Array();
+        ar.arrayCopy();
+    }
+}
---
+++ src/array/Array.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array {
+    public void arrayCopy() {
+        char[] copyFrom = { 'd', 'e', '1', 'a', 'f', 'f', 'e',
+                'i', 'n', 'a', 't', 'e', 'd' };
+        char[] copyTo = new char[7];
+
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        System.out.println(new String(copyTo));
+    }
+}
